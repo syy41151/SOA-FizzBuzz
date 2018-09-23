05d43451d3c753e29ca7c4068f6d447aa3c46dd8
syy41151 <40396787+syy41151@users.noreply.github.com>
2018-09-23 11:30:57 +0800

    Update fizzbuzz.rb

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,2 +1,17 @@
-## write your fizzbuzz method in this file
-# see http://en.wikipedia.org/wiki/Fizz_buzz for details on FizzBuzz game
+def fizzbuzz(a)
+    FIZZBUZZ.clear
+    for i in 1..a
+    FIZZBUZZ << 'FizzBuzz' if i % 3 == 0 && i % 5 == 0
+    FIZZBUZZ << 'Fizz' if i % 3 == 0 && i % 5 != 0
+    FIZZBUZZ << 'Buzz' if i % 5 == 0 && i % 3 != 0
+    FIZZBUZZ << i if i % 3 != 0 && i % 5 != 0
+    yield FIZZBUZZ[i-1] if block_given?
+    end
+    return FIZZBUZZ   
+end
+FIZZBUZZ = []
+#puts "puts FizzBuzz number?"
+#number = gets.chomp
+print fizzbuzz(5){}
+print fizzbuzz(20){|x| print "#{x}-"}
+print fizzbuzz(5){}.select{|x| x.is_a? Fixnum}
